66e48034ed1675d265e52ab5271307b2cf0b84eb
xposed01 <43813249+xposed01@users.noreply.github.com>
2022-04-26 04:13:00 +0700

    feat: infinity carousel

--- SHELTER/main.js
+++ SHELTER/main.js
@@ -1,6 +1,244 @@
+// JSON PETS DATA
+
+const petsData = [{
+        "name": "Jennifer",
+        "img": "../../assets/images/pets-jennifer.png",
+        "type": "Dog",
+        "breed": "Labrador",
+        "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
+        "age": "2 months",
+        "inoculations": ["none"],
+        "diseases": ["none"],
+        "parasites": ["none"]
+    },
+    {
+        "name": "Sophia",
+        "img": "../../assets/images/pets-sophia.png",
+        "type": "Dog",
+        "breed": "Shih tzu",
+        "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
+        "age": "1 month",
+        "inoculations": ["parvovirus"],
+        "diseases": ["none"],
+        "parasites": ["none"]
+    },
+    {
+        "name": "Woody",
+        "img": "../../assets/images/pets-woody.png",
+        "type": "Dog",
+        "breed": "Golden Retriever",
+        "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
+        "age": "3 years 6 months",
+        "inoculations": ["adenovirus", "distemper"],
+        "diseases": ["right back leg mobility reduced"],
+        "parasites": ["none"]
+    },
+    {
+        "name": "Scarlett",
+        "img": "../../assets/images/pets-scarlett.png",
+        "type": "Dog",
+        "breed": "Jack Russell Terrier",
+        "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
+        "age": "3 months",
+        "inoculations": ["parainfluenza"],
+        "diseases": ["none"],
+        "parasites": ["none"]
+    },
+    {
+        "name": "Katrine",
+        "img": "../../assets/images/pets-katrine.png",
+        "type": "Cat",
+        "breed": "British Shorthair",
+        "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
+        "age": "6 months",
+        "inoculations": ["panleukopenia"],
+        "diseases": ["none"],
+        "parasites": ["none"]
+    },
+    {
+        "name": "Timmy",
+        "img": "../../assets/images/pets-timmy.png",
+        "type": "Cat",
+        "breed": "British Shorthair",
+        "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
+        "age": "2 years 3 months",
+        "inoculations": ["calicivirus", "viral rhinotracheitis"],
+        "diseases": ["kidney stones"],
+        "parasites": ["none"]
+    },
+    {
+        "name": "Freddie",
+        "img": "../../assets/images/pets-freddie.png",
+        "type": "Cat",
+        "breed": "British Shorthair",
+        "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
+        "age": "2 months",
+        "inoculations": ["rabies"],
+        "diseases": ["none"],
+        "parasites": ["none"]
+    },
+    {
+        "name": "Charly",
+        "img": "../../assets/images/pets-charly.png",
+        "type": "Dog",
+        "breed": "Jack Russell Terrier",
+        "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
+        "age": "8 years",
+        "inoculations": ["bordetella bronchiseptica", "leptospirosis"],
+        "diseases": ["deafness", "blindness"],
+        "parasites": ["lice", "fleas"]
+    }
+]
 
 
-// BURGER-MENU
+
+//      CAROUSEL 
+
+const btnLeft = document.querySelector('#btn-left');
+const btnRight = document.querySelector('#btn-right');
+const carousel = document.querySelector('.carousel');
+
+// fn btn
+function animationBtnLeft() {
+    carousel.classList.add('transition-left');
+    btnLeft.removeEventListener('click', animationBtnLeft);
+    btnRight.removeEventListener('click', animationBtnRight);
+}
+
+function animationBtnRight() {
+    carousel.classList.add('transition-right');
+    btnRight.removeEventListener('click', animationBtnRight);
+    btnLeft.removeEventListener('click', animationBtnLeft);
+
+}
+
+// event click
+btnLeft.addEventListener('click', animationBtnLeft);
+btnRight.addEventListener('click', animationBtnRight);
+// event back end
+carousel.addEventListener('animationend', (animationEvent) => {
+
+    if (animationEvent.animationName === 'move-left') {
+        carousel.classList.remove('transition-left');
+
+        const itemsLeft = document.querySelector('#item-left').innerHTML;
+        document.querySelector('#item-active').innerHTML = itemsLeft;
+
+    } else {
+        carousel.classList.remove('transition-right');
+        const itemsRight = document.querySelector('#item-right').innerHTML;
+        document.querySelector('#item-active').innerHTML = itemsRight;
+    }
+    btnLeft.addEventListener('click', animationBtnLeft);
+    btnRight.addEventListener('click', animationBtnRight);
+    btnLeft.addEventListener('click', nonActiveGeneratorLeft);
+    btnRight.addEventListener('click', nonActiveGeneratorRight);
+});
+
+// card generator
+
+const cardPets = document.querySelectorAll('.card__pets');
+const cardPetsActive = document.querySelectorAll("#item-active > div > div");
+const cardPetsLeft = document.querySelectorAll("#item-left > div > div");
+const cardPetsRight = document.querySelectorAll("#item-right > div > div");
+
+let randomStack = [];
+let usedStack = [];
+
+// active section
+function cardGeneretor() {
+    // random generator
+    for (let i = 0; i < cardPets.length - 1; i++) {
+        randomStack.push(i);
+    }
+    console.log(randomStack)
+    // zeroing
+    for (let i = 0; i < cardPetsActive.length; i++) {
+        cardPetsActive[i].innerHTML = '';
+    }
+
+    // card generator
+    for (let i = 0; i < 3; i++) {
+        let random = randomStack.splice(Math.random() * (randomStack.length), 1)[0];
+
+        cardPetsActive[i].insertAdjacentHTML('afterbegin', `<button class="button__pets">Learn more</button>`);
+        cardPetsActive[i].insertAdjacentHTML('afterbegin', `<p>${petsData[random].name}</p>`);
+        cardPetsActive[i].insertAdjacentHTML('afterbegin', `<img src="${petsData[random].img}">`);
+    }
+};
+cardGeneretor();
+console.log(randomStack);
+
+
+// non-active sections LEFT
+function nonActiveGeneratorLeft() {
+    btnLeft.removeEventListener('click', nonActiveGeneratorLeft);
+    btnRight.removeEventListener('click', nonActiveGeneratorRight);
+
+    // zeroing
+    for (let i = 0; i < cardPetsLeft.length; i++) {
+        cardPetsLeft[i].innerHTML = '';
+    }
+    // generator
+    for (let i = 0; i < 3; i++) {
+        let random = randomStack.splice(Math.random() * (randomStack.length), 1)[0];
+        usedStack.push(random);
+        cardPetsLeft[i].insertAdjacentHTML('afterbegin', `<button class="button__pets">Learn more</button>`);
+        cardPetsLeft[i].insertAdjacentHTML('afterbegin', `<p>${petsData[random].name}</p>`);
+        cardPetsLeft[i].insertAdjacentHTML('afterbegin', `<img src="${petsData[random].img}">`);
+    }
+
+    // new diff cards
+    randomStack = [];
+    // random
+    for (let i = 0; i < cardPets.length - 1; i++) {
+        randomStack.push(i);
+    }
+    randomStack = randomStack.filter(x => !usedStack.includes(x));
+    usedStack = [];
+}
+
+// non-active sections RIGHT
+function nonActiveGeneratorRight() {
+    btnLeft.removeEventListener('click', nonActiveGeneratorLeft);
+    btnRight.removeEventListener('click', nonActiveGeneratorRight);
+
+    // zeroing
+    for (let i = 0; i < cardPetsRight.length; i++) {
+        cardPetsRight[i].innerHTML = '';
+    }
+    // generator
+    for (let i = 0; i < 3; i++) {
+        let random = randomStack.splice(Math.random() * (randomStack.length), 1)[0];
+        usedStack.push(random);
+        cardPetsRight[i].insertAdjacentHTML('afterbegin', `<button class="button__pets">Learn more</button>`);
+        cardPetsRight[i].insertAdjacentHTML('afterbegin', `<p>${petsData[random].name}</p>`);
+        cardPetsRight[i].insertAdjacentHTML('afterbegin', `<img src="${petsData[random].img}">`);
+    }
+
+    // new diff cards
+    randomStack = [];
+    // random
+    for (let i = 0; i < cardPets.length - 1; i++) {
+        randomStack.push(i);
+    }
+    randomStack = randomStack.filter(x => !usedStack.includes(x));
+    usedStack = [];
+}
+
+btnLeft.addEventListener('click', nonActiveGeneratorLeft);
+btnRight.addEventListener('click', nonActiveGeneratorRight);
+
+
+
+
+
+
+
+
+
+
+//       BURGER-MENU
 
 const menuBurger = document.querySelector('.menu-burger');
 const navigation = document.querySelector('nav');
@@ -21,18 +259,12 @@ function menuEvents() {
     document.querySelector('.blackout').classList.toggle('_blackout-active');
     // deactivate link
     activeLink.href = '#!';
-    
+    // our pets sticky header fix
     const isHeaderSticky = headerOurPets.style.position === 'sticky';
     headerOurPets.style.position = isHeaderSticky ? 'none' : 'sticky';
-
-
 }
 
 // click events
 menuBurger.addEventListener('click', menuEvents);
 navLinks.addEventListener('click', menuEvents);
 blackoutWindow.addEventListener('click', menuEvents);
-
-
-
-
